dea98c74452a7f6627d35ff5c4eb116a4e53a8b1
3Nakajugo <edinahmargarita@gmail.com>
2021-09-07 17:36:24 +0300

    add actions, reducers and middleware

---
+++ src/actions/shared.js
@@ -0,0 +1,18 @@
+import { getInitialData } from "../utils/api";
+import { receiveTweets } from "./tweets";
+import { receiveUsers } from "./users";
+import { setAuthedUser } from "./authedUser";
+
+const AUTHED_ID ='tylermcginnis'
+
+export function handleGetInitialData(){
+    return (dispatch)=>{
+        return getInitialData()
+        .then(({tweets, users})=>{ 
+            dispatch(receiveTweets(tweets))
+            dispatch(receiveUsers(users))
+            dispatch(setAuthedUser(AUTHED_ID))
+        })
+
+    }
+}
